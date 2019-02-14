3acf2d0206d114148c6be4149f3ebf4c0dd2c78b
Toni <Toni@Toni-HP>
2019-02-14 16:58:25 +0100

    Mensaje de prueba de git antonio martinez

---
+++ src/minicalculadoraejemplo/Minicalculadoraejemplo.java
@@ -0,0 +1,32 @@
+
+package minicalculadoraejemplo;
+
+/**
+ * @version 1.0 
+ * @author Toni
+ */
+public class Minicalculadoraejemplo {
+
+   
+   
+
+
+public int valorabsoluto(double numero){
+    
+   
+    
+    return (int) Math.abs(numero);
+
+    
+}
+
+public float raizcuadrada(double numero){
+    
+    
+    
+    return (float) Math.sqrt(numero);
+
+    
+}
+
+}
---
+++ src/minicalculadoraejemplo/pruebascalculadora.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minicalculadoraejemplo;
+
+/**
+ *
+ * @author Toni
+ * @version 1.0
+ */
+public class pruebascalculadora {
+
+   
+    public static void main(String[] args) {
+        
+        Minicalculadoraejemplo mc1=new Minicalculadoraejemplo();
+        
+        System.out.println("Valor absoluto: "+mc1.valorabsoluto(-3));
+        
+        System.out.println("Raiz cuadrada: "+mc1.raizcuadrada(10));
+        
+        System.out.println("Esto es una prueba de GITHUB");
+    }
+    
+}
